aedb93e93c3cf8cf59e949867099e9d731f06b78
HeeJoon-Kwon <2390kwon@g.skku.edu>
2025-05-23 14:15:28 +0900

    feat(auth): login,create-account,router

--- map/src/App.js
+++ map/src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React,{useEffect} from "react";
 import MainMap from "./pages/MainMap";
+import Login from "./pages/login";
+import CreateAccount from "./pages/create-account";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import styled from "styled-components";
+import { auth } from "./firebase";
+import { ProtectedRoute } from "./components/protected-route";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ProtectedRoute childeren={<MainMap/>} />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
+  }
+])
+
 
 function App() {
+  const init = async() => {
+    await auth.authStateReady();
+  }
+  useEffect(
+    () => {
+      init();
+    },[])
   return (
     <div>
-      <MainMap />
+      <RouterProvider router={router} />
     </div>
   );
 }
